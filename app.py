6537f2d280623a6c09fdb63c6de21df89a72e637
AnnaBeke <85964139+AnnaBeke@users.noreply.github.com>
2021-06-16 12:31:32 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+def bubble(list_a):
+    indexing_length = len(list_a) -1
+    sorted= False
+    while not sorted:
+        sorted= True
+        for i in range(0, indexing_length):
+            if list_a [i] > list_a [i+1]:
+                sorted= False
+                list_a [i], list_a [i+1] = list_a [i+1], list_a [i]
+    return list_a
+
+print(bubble([1,5,3,8,6,9,2,7]))
+
+x='Anna'
+y= 'Anna Beke'
+print(sorted(x))
+print(x.join(sorted(x)))
+print(x.isalpha())
+print(''.join(sorted(x)))
+print(''.join(sorted(y)))
+print('Nu tad beidzot!!')
+
+
+
